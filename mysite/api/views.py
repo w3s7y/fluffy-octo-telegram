4434e5cdb7aefb74ad23b1a1bf1db66e38a0b13e
Ben <ben.west.500@gmail.com>
2022-01-27 22:00:20 +0000

    Removing starter app, starting again using rest framework

--- mysite/api/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-	return HttpResponse(f"Welcome to the api app index page, you {request.method} me!")
