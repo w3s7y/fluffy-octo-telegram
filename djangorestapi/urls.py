c90973ecab1e0fafe78297ca0ae134efa2f89e57
Ben <ben.west.500@gmail.com>
2022-01-31 13:18:16 +0000

    Completed rest_framework tutorial app, snippets

--- djangorestapi/urls.py
+++ djangorestapi/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 from rest_framework import routers
 from api import views
 
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
@@ -27,6 +28,7 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('snippets/', include('snippets.urls')),
 ]
