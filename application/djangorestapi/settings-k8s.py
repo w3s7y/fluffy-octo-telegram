b24438063ed147ee2060f6294d2382ca83537898
Benjamin West <ben.west.500@gmail.com>
2023-03-11 11:47:14 +0000

    small updates

--- application/djangorestapi/settings-k8s.py
+++ application/djangorestapi/settings-k8s.py
@@ -29,8 +29,9 @@ SECRET_KEY = \
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    ".vets.internal"
+]
 
 # Application definition
 
