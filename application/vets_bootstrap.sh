ff273b8cdeae1b35efd6bc9c226dfe8f52fb0a25
Benjamin West <ben.west.500@gmail.com>
2023-11-09 01:14:05 +0000

    clean bootstrap

--- application/vets_bootstrap.sh
+++ application/vets_bootstrap.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env ash
 
 # See if migrations need to be made and apply if so.
-python3 manage.py migrate --check --no-input vets
+python3 -m application.manage migrate --check --no-input vets
 if [[ $? != 0 ]]
 then
     echo "Database had migrations that need to be applied or the db isn't ready."
     echo "Waiting 30 seconds attempting migrations"
     sleep 30
-    python3 manage.py migrate --no-input --prune vets
+    python3 -m application.manage migrate --no-input --prune vets
 fi
 
-python3 manage.py runserver $1
+python3 -m application.manage runserver $1
