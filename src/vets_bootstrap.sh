2c05363a1b2105914ffa0e0acb09fac1ff3fa509
Benjamin West <ben.west.500@gmail.com>
2023-11-09 19:40:01 +0000

    update docker bootstrap with new invocation

--- src/vets_bootstrap.sh
+++ src/vets_bootstrap.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env ash
 
 # See if migrations need to be made and apply if so.
-vets-app-manage migrate --check --no-input vets
+python -m manage migrate --check --no-input vets
 if [[ $? != 0 ]]
 then
     echo "Database had migrations that need to be applied or the db isn't ready."
     echo "Waiting 30 seconds attempting migrations"
     sleep 30
-    vets-app-manage migrate --no-input --prune vets
+    python -m manage migrate --no-input --prune vets
 fi
 
-vets-app-manage runserver $1
+python -m manage $1
